744bec90448502788b876194e763ec010f2dc697
Lyle Mitchell <ljwmitchell@gmail.com>
2018-05-11 14:49:54 +0100

    Adds Item class with getters & setters

---
+++ src/main/java/Item.java
@@ -0,0 +1,46 @@
+public abstract class Item  {
+
+    private String name;
+    private String description;
+    private double cost;
+    private double retail;
+
+    public Item(String name, String description, double cost, double retail) {
+        this.name = name;
+        this.description = description;
+        this.cost = cost;
+        this.retail = retail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public double getRetail() {
+        return retail;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public void setRetail(double retail) {
+        this.retail = retail;
+    }
+}
