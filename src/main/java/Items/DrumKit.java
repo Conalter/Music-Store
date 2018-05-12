bd5e6d84119530a19447468c273b815fa2967b63
Lyle Mitchell <ljwmitchell@gmail.com>
2018-05-12 10:20:48 +0100

    adds DrumKitTest tests

--- src/main/java/Items/DrumKit.java
+++ src/main/java/Items/DrumKit.java
@@ -1,28 +1,33 @@
-//package Items;
-//
-//import playable.Playable;
-//
-//public class DrumKit extends MusicalInstrument implements Playable{
-//
-//    private FamilyType family;
-//    private int skins;
-//    private int sticks;
-//
-//    public DrumKit(String name, String description, double cost, double retail, FamilyType family, int skins, int sticks) {
-//        super(name, description, cost, retail, family);
-//        this.skins = skins;
-//        this.sticks = skins;
-//    }
-//
-//    public int getSkins() {
-//        return skins;
-//    }
-//
-//    public int getSticks() {
-//        return sticks;
-//    }
-//
-//    public String Play(){
-//        return "bish bash goes the drums";
-//    }
-//}
+package Items;
+
+import playable.Playable;
+
+public class DrumKit extends MusicalInstrument implements Playable{
+
+    private FamilyType family;
+    private int skins;
+    private int sticks;
+
+    public DrumKit(String name, String description, double cost, double retail, FamilyType family, int skins, int sticks) {
+        super(name, description, cost, retail, family);
+        this.skins = skins;
+        this.sticks = sticks;
+        this.family = family;
+    }
+
+    public int getSkins() {
+        return skins;
+    }
+
+    public int getSticks() {
+        return sticks;
+    }
+
+    public FamilyType getFamily(){
+        return this.family;
+    }
+
+    public String play(){
+        return "bish bash goes the drums";
+    }
+}
--- src/test/java/DrumKitTest.java
+++ src/test/java/DrumKitTest.java
@@ -1,12 +1,26 @@
-//import Items.DrumKit;
-//import org.junit.Before;
-//
-//public class DrumKitTest {
-//
-//    DrumKit drumkit;
-//
-//    @Before
-//    public void before(){
-//        drumkit = new DrumKit("Ludwig Centennial Zep Set", "High-quality shell pack from Ludwig.", 500.00, 1300.00, )
-//    }
-//}
+import Items.DrumKit;
+import Items.FamilyType;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DrumKitTest {
+
+    DrumKit drumkit;
+
+    @Before
+    public void before(){
+        drumkit = new DrumKit("Ludwig Centennial Zep Set", "High-quality shell pack from Ludwig.", 500.00, 1300.00, FamilyType.PERCUSSION, 10, 12);
+    }
+
+    @Test
+    public void canGetSkins(){
+        assertEquals(10, drumkit.getSkins());
+    }
+
+    @Test
+    public void canGetSticks(){
+        assertEquals(12, drumkit.getSticks());
+    }
+}
